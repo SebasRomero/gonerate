c18465c9fbc399a7814ec8465e278e51d2671c7b
Sebastian <sebastian@coderlabs.co>
2024-01-14 15:16:44 -0500

    feat:: add main generator and server init

--- cmd/generator/file-generator/files.go
+++ cmd/generator/file-generator/files.go
@@ -10,12 +10,12 @@ func InitProject(args []string, typeApi string) {
 	if typeApi == string(types.Rest) { //TODO Rest
 		projectName := args[0]
 		createFolder(getCurrentWorkDirectory(projectName), args[0])
-		createFile(getCurrentWorkDirectory(projectName), args[0])
+		createMain(getCurrentWorkDirectory(projectName))
 
 	} else { //TODO Graph
 		projectName := args[0]
 		createFolder(getCurrentWorkDirectory(projectName), args[0])
-		createFile(getCurrentWorkDirectory(projectName), args[0])
+		createMain(getCurrentWorkDirectory(projectName))
 
 	}
 }
@@ -29,9 +29,16 @@ func getCurrentWorkDirectory(projectName string) string {
 	return currentDirectory
 }
 
-func createFile(currentDirectory string, projectName string) {
-	newFile := currentDirectory + projectName + ".go"
-	os.Create(newFile)
+func createMain(currentDirectory string) {
+	newFile := currentDirectory + "main.go"
+	file, err := os.Create(newFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writting := []byte(initServerWriter())
+	file.Write(writting)
+
 }
 
 func createFolder(currentDirectory string, projectName string) {
---
+++ cmd/generator/file-generator/write.go
@@ -0,0 +1,29 @@
+package filegenerator
+
+func initServerWriter() string {
+	return `
+	package main
+	
+	func main(){
+	ctx := context.Background()
+	serverDoneChan := make(chan os.Signal, 1)
+
+	signal.Notify(serverDoneChan, os.Interrupt, syscall.SIGTERM)
+
+	srv := server.New(":8080")
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	fmt.Println("Server running on port", srv.Addr)
+	<-serverDoneChan
+
+	srv.Shutdown(ctx)
+	log.Println("Server stopped")
+	}
+	`
+}
